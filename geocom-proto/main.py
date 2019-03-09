3043eacd3417c54e89ec9216889d53e95668ac14
peterpitovsky <petr.gezhes@diginetica.com>
2019-03-09 17:56:59 +0300

    fixes and cancel

--- geocom-proto/main.py
+++ geocom-proto/main.py
@@ -1,17 +1,22 @@
 import socket, select, sys
 import argparse
 import queue, threading
+import time
 from threading import Thread, Lock
 
 def make_request(rpc, trId=None, params=[]):
     result = "\n%R1Q," + str(rpc)
     if trId != None:
-        result += "," + str(trId)
+        result += "," + str(trId+1)
     result += ":"
     result += ','.join(map(str, params))
     result += "\r\n"
     print(result)
     return bytes(result, "ascii")
+
+def make_cancel_request():
+    print('cancelling')
+    return bytes("\nc\r\n", "ascii")
 
 def parse_response(data):
     data = data.decode("ascii")
@@ -25,14 +30,18 @@ def parse_response(data):
           "P":      body[1:-1],
     }
 
-outputs = [queue.Queue(1), queue.Queue(1)]
+outputs = [queue.Queue(), queue.Queue()]
 
-def do(sock, q, rpc, params):
-    req = make_request(rpc, q, params)
+def do_request(sock, tid, rpc, params=[]):
+    req = make_request(rpc, tid, params)
+    sock.send(req)
+
+def do_cancel_request(sock):
+    req = make_cancel_request()
     sock.send(req)
 
 parser = argparse.ArgumentParser(description='MS60 CLI')
-parser.add_argument('--host',   default='192.168.43.131', metavar='MS60 host')
+parser.add_argument('--host',   default='192.168.12.59',  metavar='MS60 host')
 parser.add_argument('--port',   default=1212,             metavar='MS60 port')
 parser.add_argument('--rpc',    default=0,                metavar='RPC number')
 parser.add_argument('--params', default="",               metavar='RPC parameters')
@@ -57,10 +66,28 @@ reader = threading.Thread(target=receive_responses)
 reader.start()
 
 params = args.params.split(',') if len(args.params) > 0 else []
-out1 = do(sock, 1, args.rpc, params)
-out2 = do(sock, 2, args.rpc, params)
-print(outputs[0].get())
-print(outputs[1].get())
+#out1 = do_request(sock, 0, args.rpc, params)
+#out2 = do_request(sock, 1, args.rpc, params)
+#print(outputs[0].get())
+#print(outputs[1].get())
+
+do_request(sock, 0, rpc=0)
+time.sleep(1)
+do_cancel_request(sock)
+#do_request(sock, 0, 9029, params=[1,1,0]) # search
+do_request(sock, 0, 9027, params=[-0.15, 0.52, '0', '0', '0']) # turn
+for i in range(10):
+    do_request(sock, 1, 17017, params=[2])
+print('sent')
+for i in range(10):
+    print(outputs[1].get())
+#make_cr(9027, params=[0.15, 0.55, '0', '0', '0'])
+#time.sleep(3)
+#make_cr(17019, params=[1])
+#make_cr(17021, params=[1])
+#make_cr(17017, params=[2])
+#make_cr(17019, params=[5])
+#time.sleep(3)
 
 sock.close()
 reader.join()
