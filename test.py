1df3ab95468e7b3216d309f58468c36d9378420e
Polina Matavina <paulin@yandex-team.ru>
2019-03-09 17:42:37 +0300

    add tracks to the image

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 from flask import Flask, render_template, Response
 
+
 class VideoCamera(object):
     def __init__(self, width, height):
         # Using OpenCV to capture from device 0. If you have trouble capturing
@@ -14,18 +15,74 @@ class VideoCamera(object):
         self.video = cv2.VideoCapture(1) # 0 is laptop webcam, 1 is external
         self.video.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
+
+        self.feature_params = {
+            'maxCorners': 100,
+            'qualityLevel': 0.3,
+            'minDistance': 7,
+            'blockSize': 7
+        }
+
+        # Parameters for lucas kanade optical flow
+        self.lk_params = {
+            'winSize': (15, 15),
+            'maxLevel': 2,
+            'criteria': (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
+        }
+        self._init_state()
+
+    def _init_state(self):
+        # Create some random colors
+        self.color = np.random.randint(0, 255, (100, 3))
+
+        # Take first frame and find corners in it
+        _, self.old_frame = self.video.read()
+        self.old_gray = cv2.cvtColor(self.old_frame, cv2.COLOR_BGR2GRAY)
+        self.p0 = cv2.goodFeaturesToTrack(self.old_gray, mask=None, **self.feature_params)
+
+        # Create a mask image for drawing purposes
+        self.mask = np.zeros_like(self.old_frame)
     
     def __del__(self):
         self.video.release()
     
     def get_frame(self):
-        success, image = self.video.read()
-        return image
+        success, frame = self.video.read()
 
-class AugumentedSpace(object):
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # calculate optical flow
+        p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_gray, frame_gray, self.p0, None, **self.lk_params)
+
+        # Select good points
+        if p1 is None:
+            self._init_state()
+            return frame
+
+        good_new = p1[st==1]
+        good_old = self.p0[st==1]
+
+        # draw the tracks
+        for i,(new,old) in enumerate(zip(good_new, good_old)):
+            a,b = new.ravel()
+            c,d = old.ravel()
+            self.mask = cv2.line(self.mask, (a, b), (c, d), self.color[i].tolist(), 2)
+            frame = cv2.circle(frame, (a, b), 5, self.color[i].tolist(), -1)
+        img = cv2.add(frame, self.mask)
+
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            self._init_state()
+            return img
+
+        # Now update the previous frame and previous points
+        self.old_gray = frame_gray.copy()
+        self.p0 = good_new.reshape(-1, 1, 2)
+
+        return img
+
+
+class AugmentedSpace(object):
     def __init__(self, width, height):
         fuze_trimesh = trimesh.load('teapot.obj')
         mesh = pyrender.Mesh.from_trimesh(fuze_trimesh) #, material=
@@ -52,6 +109,7 @@ class AugumentedSpace(object):
         color, depth = self.renderer.render(self.scene)
         return color, depth
 
+
 def cv_to_jpg(image):
     # We are using Motion JPEG, but OpenCV defaults to capture raw images,
     # so we must encode it into JPEG in order to correctly display the
@@ -59,15 +117,20 @@ def cv_to_jpg(image):
     ret, jpeg = cv2.imencode('.jpg', image)
     return jpeg.tobytes()
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 def gen(camera, ar):
     while True:
         frame = camera.get_frame()
+        if frame is None:
+            continue
         ar_color, ar_depth = ar.get_frame()
         mask = np.repeat(np.expand_dims(ar_depth > 0.01, -1), 3, axis=-1) # 3 channels
         frame = np.where(mask, ar_color, frame)
@@ -75,12 +138,14 @@ def gen(camera, ar):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + jpg + b'\r\n\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
     w = 1280
     h = 720
-    return Response(gen(VideoCamera(w, h), AugumentedSpace(w, h)),
+    return Response(gen(VideoCamera(w, h), AugmentedSpace(w, h)),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2122, debug=True)
